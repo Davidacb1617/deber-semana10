f070702778bba3372fb3f99c2a0e5e8a334e15c4
Davidacb1617 <dacorrea@sudamericano.edu.ec>
2023-12-13 23:30:02 -0500

    delete (prueba)

--- deber_str.js
+++ deber_str.js
@@ -64,3 +64,11 @@ alert(url.substring(url.indexOf("/")+1));
 
 let url1 = "https://drive.google.com/file/d/1FP3paEh4-Z6ExNQ_Lp4N9WO7XQ_3DPG1/view?usp=drive_link"
 alert(url1.substring(url1.indexOf("d/")+2, url1.indexOf("/", 44)));
+
+
+// EJERCICIO 8
+
+// Entrada: LAB2#B
+// Salida: C
+
+let 
--- prueba.js
+++
@@ -1,7 +0,0 @@
-//EJERCICIO 7
-
-// Entrada: https://drive.google.com/file/d/1FP3paEh4-Z6ExNQ_Lp4N9WO7XQ_3DPG1/view?usp=drive_link
-// Salida: 1FP3paEh4-Z6ExNQ_Lp4N9WO7XQ_3DPG1
-
-let url1 = "https://drive.google.com/file/d/1FP3paEh4-Z6ExNQ_Lp4N9WO7XQ_3DPG1/view?usp=drive_link"
-alert(url1.substring(url1.indexOf("d/")+2, url1.indexOf("/", 32)));
