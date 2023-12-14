0f42e3418f339b00ab7a1ee3963bceefdaff4437
Davidacb1617 <dacorrea@sudamericano.edu.ec>
2023-12-13 23:29:15 -0500

    more exercises

--- deber_str.js
+++ deber_str.js
@@ -57,3 +57,10 @@ alert(correo.substring(0, correo.indexOf("@")));
 let url = "facebook.com/nombreuser";
 alert(url.substring(url.indexOf("/")+1));
 
+//EJERCICIO 7
+
+// Entrada: https://drive.google.com/file/d/1FP3paEh4-Z6ExNQ_Lp4N9WO7XQ_3DPG1/view?usp=drive_link
+// Salida: 1FP3paEh4-Z6ExNQ_Lp4N9WO7XQ_3DPG1
+
+let url1 = "https://drive.google.com/file/d/1FP3paEh4-Z6ExNQ_Lp4N9WO7XQ_3DPG1/view?usp=drive_link"
+alert(url1.substring(url1.indexOf("d/")+2, url1.indexOf("/", 44)));
